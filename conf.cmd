b2d74ca9aa8aabe5da7d86a907edcca397b94593
Paul Smedley <paul@smedley.id.au>
2022-03-25 19:29:34 +1030

    Fix paths in conf.cmd

--- conf.cmd
+++ conf.cmd
@@ -1,5 +1,5 @@
 set config_site=u:/moztools/config.site-gcc
-set path=u:\dev\gcc-11.2.0\host-i386-pc-os2-emx\gcc;%path%
+set path=u:\dev\gcc-11-os2\host-i386-pc-os2-emx\gcc;%path%
 set LIBS= -lpthread
 set grep=grep.exe
 #ash ./configure --prefix=/usr/local1120 --enable-shared --enable-languages=c,c++,fortran --with-gnu-as --enable-frame-pointer --disable-bootstrap --disable-libstdcxx-pch  --enable-threads --build=i686-pc-os2-emx --target=i686-pc-os2-emx 2>&1 | tee configure.log
