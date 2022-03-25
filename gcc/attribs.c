838baeb62bac9c447b178c8c3874edb8b7be1d5b
Paul Smedley <paul@smedley.id.au>
2022-03-25 19:30:44 +1030

    Revert change that causes SIGSEGV in cc1.exe on some source files

--- gcc/attribs.c
+++ gcc/attribs.c
@@ -707,7 +707,6 @@ decl_attributes (tree *node, tree attributes, int flags,
 	continue;
 
       if (spec->handler != NULL)
-#if 0
 	{
 	  int cxx11_flag = (cxx11_attr_p ? ATTR_FLAG_CXX11 : 0);
 
@@ -734,25 +733,6 @@ decl_attributes (tree *node, tree attributes, int flags,
 	  else
 	    returned_attrs = chainon (ret, returned_attrs);
 	}
-#else
-        {
-	  int cxx11_flag = (cxx11_attr_p ? ATTR_FLAG_CXX11 : 0);
-
-          /* We may have replaced decl with its type (see above).  Supply the
-             original decl to the handler following the replacing type.  This is
-             used by handlers of some targets to alter function decls depending
-             on function type attributes.  */
-          tree nodes[2];
-          nodes[0] = *anode;
-          nodes[1] = *node;
-          returned_attrs = chainon ((*spec->handler) (nodes, name, args,
-                                                      flags | cxx11_flag | ATTR_FLAG_HANDLER_DECL_FOLLOWS,
-                                                      &no_add_attrs),
-                                    returned_attrs);
-          /* The node could be changed by the handler, preserve the change.  */
-          *anode = nodes[0];
-        }
-#endif
 
       /* Layout the decl in case anything changed.  */
       if (spec->type_required && DECL_P (*node)
