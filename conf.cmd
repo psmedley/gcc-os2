adfb7a706fa4cd05fdd0da6f238f554edbe5a975
Paul Smedley <paul@smedley.id.au>
2022-04-21 19:05:59 +0930

    Update source to 11.3.0

--- conf.cmd
+++ conf.cmd
@@ -2,7 +2,7 @@ set config_site=u:/moztools/config.site-gcc
 set path=u:\dev\gcc-11-os2\host-i386-pc-os2-emx\gcc;%path%
 set LIBS= -lpthread
 set grep=grep.exe
-#ash ./configure --prefix=/usr/local1120 --enable-shared --enable-languages=c,c++,fortran --with-gnu-as --enable-frame-pointer --disable-bootstrap --disable-libstdcxx-pch  --enable-threads --build=i686-pc-os2-emx --target=i686-pc-os2-emx 2>&1 | tee configure.log
-ash ./configure --prefix=/usr/local1120 --enable-shared --enable-languages=c,c++,fortran --with-gnu-as --enable-frame-pointer --disable-bootstrap --disable-libstdcxx-pch  --enable-threads --enable-decimal-float=yes --build=i686-pc-os2-emx --target=i686-pc-os2-emx 2>&1 | tee configure.log
+#ash ./configure --prefix=/usr/local1130 --enable-shared --enable-languages=c,c++,fortran --with-gnu-as --enable-frame-pointer --disable-bootstrap --disable-libstdcxx-pch  --enable-threads --build=i686-pc-os2-emx --target=i686-pc-os2-emx 2>&1 | tee configure.log
+ash ./configure --prefix=/usr/local1130 --enable-shared --enable-languages=c,c++,fortran --with-gnu-as --enable-frame-pointer --disable-bootstrap --disable-libstdcxx-pch  --enable-threads --enable-decimal-float=yes --build=i686-pc-os2-emx --target=i686-pc-os2-emx 2>&1 | tee configure.log
 
 # fix failed assertion in gcc\gcc.c
